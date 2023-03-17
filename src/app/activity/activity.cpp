9a431e8b6e51ef6d0c3aacf15070cb84ac8186ac
rapsakka <tapani.parssinen@aalto.fi>
2023-03-17 13:44:26 +0000

    Temperature added

--- src/app/activity/activity.cpp
+++ src/app/activity/activity.cpp
@@ -24,6 +24,13 @@
 // #include "hardware/blestepctl.h"
 #include "hardware/motor.h"
 
+#include <iostream>
+#include <chrono>
+#include <string> 
+#include <sstream>
+#include <iostream>
+#include <cstring>
+
 #ifdef NATIVE_64BIT
 
 #else
@@ -43,15 +50,29 @@ LV_FONT_DECLARE(Ubuntu_32px);
 #define YES "Yes"
 #define NO  "No"
 
+#define SERVICE_UUID  "bc2fa7bd-60cd-46f6-bf7f-3ea374234003"
+#define CHARACTERISTIC_UUID "73c351d2-ee74-4f5e-b5d3-03a56553fed4"
+
 static SynchronizedApplication activityApp;
 static JsonConfig config("activity.json");
 
 // Options
-static String size, length, goal_step, goal_dist;
+static String length, goal_step, goal_dist, weight, goal_calorie; //weight and calorie_goal added 
+
 // Widgets
 static Label lblStepcounter, lblStepachievement;
 static Label lblDistance, lblDistachievement;
-static Arc arcStepcounter, arcDistance;
+static Arc arcStepcounter, arcDistance, arcCalorie; //arcCalorie added
+static Label lblCalorie, lblCaloachievement; //added 
+
+//Globals
+static float calorie,temp;
+uint32_t stp;
+uint32_t dist;
+std::string message;
+static std::time_t start_time, end_time, r_time;
+
+static bool Sync  = true;
 
 static Style big, small;
 
@@ -65,6 +86,59 @@ static void build_settings();
 static void activity_activate_cb();
 static void activity_reset_cb(lv_obj_t * obj, lv_event_t event);
 
+
+
+float getTemp(){
+
+TTGOClass *ttgo = TTGOClass::getWatch();
+    
+    return ttgo->bma->temperature();
+}
+
+std::string combine(){
+
+    std::stringstream s;
+    std::string resultant;
+
+    s << Sync;
+    s << ",";
+
+    s<< r_time;
+    s << ",";
+    
+    s<< end_time;
+    s << ",";
+    
+    s << stp;
+    s << ",";
+    
+    s << dist;
+    s << ",";
+    
+    s << calorie;
+    s << ",";
+
+    s << temp;
+
+    s >> resultant;
+    return resultant;
+}
+
+class UpdateStats : public NimBLECharacteristicCallbacks{
+    void onRead(NimBLECharacteristic    *pCharacteristic){
+        
+        if(Sync){
+            pCharacteristic->setValue( message );
+        }
+        else if (!Sync){
+            
+            pCharacteristic->setValue( message );
+        }
+    }
+};
+
+
+
 /*
  * setup routine for application
  */
@@ -72,7 +146,7 @@ void activity_app_setup() {
     // Create and register new application
     //   params: name, icon, auto add "refresh" button (this app will use synchronize function of the SynchronizedApplication class).
     //   Also, you can configure count of the required pages in the next two params (to have more app screens).
-    activityApp.init("activity", &move_64px, 1, 1);
+    activityApp.init("hiking", &move_64px, 1, 1);
 
     mainbar_add_tile_activate_cb( activityApp.mainTileId(), activity_activate_cb );
 
@@ -82,11 +156,12 @@ void activity_app_setup() {
 
     // Executed when user click "refresh" button
     activityApp.synchronizeActionHandler([](SyncRequestSource source) {
-        if ( blectl_get_event( BLECTL_ON ) )
-        {
-            // blestepctl_update(true);
-        }
-        // Return feedback to user as nothing else changed
+        
+        //refresh_progress(false); //refresh ongoing session
+        Sync = true;
+        message = combine();
+        //SOME SENDING/WRITING FUNCTION HERE
+
         motor_vibe(20);
     });
 
@@ -104,13 +179,30 @@ void activity_app_setup() {
         lv_obj_align(mbox1, NULL, LV_ALIGN_CENTER, 0, 0);
     });
     
+    NimBLEDevice::init("Hiking");
+    
+    NimBLEServer *pServer = NimBLEDevice::createServer();
+    NimBLEService *pService = pServer->createService(SERVICE_UUID);
+    NimBLECharacteristic *pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE );
+    pService->start();
+
+    pCharacteristic -> setCallbacks(new UpdateStats());
+    pCharacteristic->setValue(0);
+
+    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID); 
+    pAdvertising->start(); 
+    start_time = std::time(0);
+    r_time = std::time(0);
+    temp = getTemp();
     refresh_main_page();
+    
 }
 
 void build_main_page()
 {
     big = Style::Create(ws_get_mainbar_style(), true);
-    big.textFont(&Ubuntu_32px)
+    big.textFont(&Ubuntu_16px)
       .textOpacity(LV_OPA_80);
     small = Style::Create(ws_get_mainbar_style(), true);
     small.textFont(&Ubuntu_16px)
@@ -122,7 +214,7 @@ void build_main_page()
     arcStepcounter.start(0).end(0).rotation(90)
         .style(ws_get_arc_style(), LV_ARC_PART_INDIC, false )
         .style(ws_get_arc_bg_style(), LV_ARC_PART_BG, false )
-        .size(lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 2 : lv_disp_get_ver_res( NULL ) / 2, lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 2 : lv_disp_get_ver_res( NULL ) / 2 )
+        .size(lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3, lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3 )
         .alignInParentLeftMid(0, 0);
 
     lblStepcounter = Label(&screen);
@@ -139,8 +231,8 @@ void build_main_page()
     arcDistance.start(0).end(0).rotation(90)
         .style(ws_get_arc_style(), LV_ARC_PART_INDIC, false )
         .style(ws_get_arc_bg_style(), LV_ARC_PART_BG, false )
-        .size(lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 2 : lv_disp_get_ver_res( NULL ) / 2, lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 2 : lv_disp_get_ver_res( NULL ) / 2 )
-        .alignInParentRightMid(0, 0);
+        .size(lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3 , lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3 )
+        .alignInParentCenter(0, 0);
 
     lblDistance = Label(&screen);
     lblDistance.text("0")
@@ -151,17 +243,52 @@ void build_main_page()
     lblDistachievement.text("0")
         .style(small, true)
         .alignOrig0(arcDistance, LV_ALIGN_CENTER);
+
+    // ==============  ADDED  ===============
+    arcCalorie= Arc(&screen, 0, 360);
+    arcCalorie.start(0).end(0).rotation(90)
+        .style(ws_get_arc_style(), LV_ARC_PART_INDIC, false )
+        .style(ws_get_arc_bg_style(), LV_ARC_PART_BG, false )
+        .size(lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3 , lv_disp_get_ver_res( NULL ) > lv_disp_get_hor_res( NULL ) ? lv_disp_get_hor_res( NULL ) / 3 : lv_disp_get_ver_res( NULL ) / 3 )
+        .alignInParentRightMid(0, 0);
+
+    lblCalorie = Label(&screen);
+    lblCalorie.text("0")
+        .style(big, true)
+        .align(arcCalorie, LV_ALIGN_OUT_TOP_MID);
+    
+    lblCaloachievement = Label(&screen);
+    lblCaloachievement.text("0")
+        .style(small, true)
+        .alignOrig0(arcCalorie, LV_ALIGN_CENTER);
 }
 
 void refresh_main_page()
-{
+{   
     char buff[36];
     uint32_t gStep = atoi( goal_step.c_str() );
     uint32_t gDist = atoi( goal_dist.c_str() );
+    uint32_t gCal = atoi( goal_calorie.c_str() );
     // Get current value
-    uint32_t stp = bma_get_stepcounter();
+    stp = bma_get_stepcounter();
     uint32_t ach = gStep == 0 ? 0 : 100 * stp / gStep;
-    uint32_t dist = stp * atoi( length.c_str() ) / 100;
+    dist = stp * atoi( length.c_str() ) / 100;
+    
+    /// ===========  ADDED  =========
+    std::time_t now = std::time(0); 
+    double dif_seconds = difftime(now,start_time);
+    std::stringstream ss; 
+    ss<< weight;
+    int w ;
+    ss >> w ;
+    calorie = dif_seconds* 21 * w/(12000) ; //calorie counting T × MET × 3.5 × W / (200 × 60) 
+
+    if( uint32_t(dif_seconds) % 10 == 0){ //every 10s get running average of temperature
+
+    temp = (temp+getTemp())/2;
+
+    }
+    /// ===========  ADDED  =========
     log_d("Refresh activity: %d steps", stp);
     // Raw steps
     snprintf( buff, sizeof( buff ), "%d", stp );
@@ -177,6 +304,15 @@ void refresh_main_page()
     snprintf( buff, sizeof( buff ), "%d%%", gDist == 0 ? 0 : 100 * dist / gDist );
     lblDistachievement.text(buff).realign();
     arcDistance.end( gDist == 0 ? 0 : 360 * dist / gDist );
+
+    //=============  ADDED  ============
+    //Calories 
+    snprintf( buff, sizeof( buff ), "%.1f kcal", calorie );
+    lblCalorie.text(buff).realign();
+    // Achievement
+    snprintf( buff, sizeof( buff ), "%.0f%%", gCal == 0 ? 0 : 100 * calorie / gCal );
+    lblCaloachievement.text(buff).realign();
+    arcCalorie.end( gCal == 0 ? 0 : 360 * calorie / gCal );
 }
 
 void activity_activate_cb()
@@ -188,9 +324,25 @@ static void activity_reset_cb(lv_obj_t * obj, lv_event_t event)
 {
     if(event == LV_EVENT_VALUE_CHANGED) {
         const char *answer = lv_msgbox_get_active_btn_text(obj);
-        if ( strcmp( answer, YES) == 0 ) {
+        
+        if ( strcmp( answer, YES) == 0 ) { // STOP HAS BEEN PRESSED
+            Sync = false;
+    
             /* Reset counter */
-            bma_reset_stepcounter();
+            end_time = std::time(0);
+            message = combine();
+            
+            bma_reset_stepcounter(); 
+            temp = getTemp();
+            r_time = start_time;
+            start_time = std::time(0);
+            /*
+            std::string mes  ("Start time: ");
+            const int length = mes.length();
+            char* message = new char[length + 1];
+            strcpy( message, mes.c_str() );
+            const char* m = const_cast<char*>(message);
+            */
             /* Refresh display immediately for user feedback */
             refresh_main_page();
         }
@@ -198,13 +350,13 @@ static void activity_reset_cb(lv_obj_t * obj, lv_event_t event)
     /* Call the default callback to retrieve the default behavior */
     default_msgbox_cb(obj, event);
 }
-
 void build_settings()
 {
     // Create full options list and attach items to variables
     config.addString("Step length (cm)", 3, "50").setDigitsMode(true,"0123456789").assign(&length); // cm
     config.addString("Step Goal", 7, "10000").setDigitsMode(true,"0123456789").assign(&goal_step); // steps
     config.addString("Distance Goal", 7, "5000").setDigitsMode(true,"0123456789").assign(&goal_dist); // m
-
+    config.addString("Your weight", 3, "70").setDigitsMode(true,"0123456789").assign(&weight); //kg
+    config.addString("Calorie Goal", 5, "100").setDigitsMode(true,"0123456789").assign(&goal_calorie); //kcal
     activityApp.useConfig(config, true); // true - auto create settings page widgets
 }
